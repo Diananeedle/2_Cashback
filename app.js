a0601c6087185e019a83c21a3d38cdc53a9f0658
diananeedle <diananeedle@gmail.com>
2019-10-10 16:44:09 +0400

    Add if/else

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.groupCollapsed('worked');
+console.log('worked');
+
+const purchase = 12500;
+let cashback;
+
+const regularPercent = 0.01;
+const increasedPercent = 0.05;
+const specialPercent = 0.3;
+
+if (purchase < 1000){
+    cashback = purchase*regularPercent;
+} else if (purchase < 10000) {
+    cashback = purchase*increasedPercent;
+} else {
+    cashback = purchase*specialPercent;
+}
+
+if (cashback >= 3000) {
+    console.log ("Your cashback: ", 3000);
+} else {
+    console.log(cashback);
+}
