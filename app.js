b1f644f3104a28e67f1f423a8db25cc201659db5
diananeedle <diananeedle@gmail.com>
2019-11-04 17:03:07 +0400

    #2

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 console.log('worked');
 
-const purchase = 12500;
 let cashback;
 
 const regularPercent = 0.01;
 const increasedPercent = 0.05;
 const specialPercent = 0.3;
 
-if (purchase < 1000){
-    cashback = purchase*regularPercent;
-} else if (purchase < 10000) {
-    cashback = purchase*increasedPercent;
-} else {
-    cashback = purchase*specialPercent;
-}
+const regularAmount = 100;
+const increasedAmount = 1000;
+const specialAmount = 9000;
+
+cashback = regularPercent*regularAmount + increasedPercent*increasedAmount + specialPercent*specialAmount;
 
 if (cashback >= 3000) {
     console.log ("Your cashback: ", 3000);
